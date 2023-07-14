9892e4df92aec619626cf35064fc3260772b14b9
Abdelkarim Elmostakim <abdelkarimelmostakim07@gmail.com>
2023-07-13 19:09:23 -0700

    Hello, betty style

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,12 +3,14 @@
 /**
  * main - Entry point
  * 
- * Return - Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-    printf("with proper grammar, but the outcome is a piece of art,\n");
+    printf("with proper grammar, but the outcome is a piece of art,");
+    
+    printf("\n");
 
-    return 0;
+    return (0);
 }
