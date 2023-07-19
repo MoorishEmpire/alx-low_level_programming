3498d6c89b8dcef436168ee31cd305348c55c89e
Abdelkarim Elmostakim <abdelkarimelmostakim07@gmail.com>
2023-07-18 19:59:18 -0700

    Jack Bauer

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <limits.h> // Include <limits.h> to use INT_MIN
 
 /**
  * print_last_digit - Prints the last digit of a number
@@ -8,13 +9,25 @@
  */
 int print_last_digit(int n)
 {
-int last_digit;
+    int last_digit;
 
-if (n < 0)
-n = -n;
+    if (n == 0)
+    {
+        _putchar('0');
+        return (0);
+    }
+    else if (n == INT_MIN) // Check if n is equal to INT_MIN directly
+    {
+        _putchar('0' + 8); // The last digit of INT_MIN is 8 (in absolute value)
+        return (8);
+    }
 
-last_digit = n % 10;
-_putchar('0' + last_digit);
+    if (n < 0)
+    {
+        n = -n; // Make n positive
+    }
 
-return (last_digit);
+    last_digit = n % 10;
+    _putchar('0' + last_digit);
+    return (last_digit);
 }
