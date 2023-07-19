08e6cc665b7a34f4a8d025557d25c73814c8c3f2
Abdelkarim Elmostakim <abdelkarimelmostakim07@gmail.com>
2023-07-18 20:03:01 -0700

    Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,11 +11,11 @@ for (hour = 0; hour < 24; hour++)
 {
 for (minute = 0; minute < 60; minute++)
 {
-_putchar('0' + hour / 10); 
-_putchar('0' + hour % 10); 
+_putchar('0' + hour / 10); /* Print tens digit of hour */
+_putchar('0' + hour % 10); /* Print units digit of hour */
 _putchar(':');
-_putchar('0' + minute / 10); 
-_putchar('0' + minute % 10); 
+_putchar('0' + minute / 10); /* Print tens digit of minute */
+_putchar('0' + minute % 10); /* Print units digit of minute */
 _putchar('\n');
 }
 }
