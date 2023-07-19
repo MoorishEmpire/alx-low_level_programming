b6527f9935490142db4f88200e23c022e34753fc
Abdelkarim Elmostakim <abdelkarimelmostakim07@gmail.com>
2023-07-18 19:22:23 -0700

    Printing the last digit

---
+++ 0x02-functions_nested_loops/7-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    print_last_digit(98);
+    print_last_digit(0);
+    print_last_digit(-1024);
+    _putchar('\n');
+    return (0);
+}
+
