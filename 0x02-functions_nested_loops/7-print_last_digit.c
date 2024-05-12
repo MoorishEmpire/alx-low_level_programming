8a66207fd55f7eea892b09130d82c24e06888351
Abdelkarim Elmostakim <abdelkarimelmostakim07@gmail.com>
2024-05-11 17:22:52 -0700

    last digit putchar

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,33 +1,19 @@
 #include "main.h"
-#include <limits.h> // Include <limits.h> to use INT_MIN
+#include <stdlib.h>
 
 /**
- * print_last_digit - Prints the last digit of a number
- * @n: The number to find the last digit of
- *
- * Return: The value of the last digit
+	* print_last_digit - Prints the last digit of a number
+	* @n: The number to find the last digit of
+	*
+	* Return: The value of the last digit
  */
+
 int print_last_digit(int n)
 {
-    int last_digit;
+	n = abs(n);
+	n = n % 10;
+	_putchar(n + '0');
 
-    if (n == 0)
-    {
-        _putchar('0');
-        return (0);
-    }
-    else if (n == INT_MIN) // Check if n is equal to INT_MIN directly
-    {
-        _putchar('0' + 8); // The last digit of INT_MIN is 8 (in absolute value)
-        return (8);
-    }
-
-    if (n < 0)
-    {
-        n = -n; // Make n positive
-    }
-
-    last_digit = n % 10;
-    _putchar('0' + last_digit);
-    return (last_digit);
+	return (n);
 }
+
