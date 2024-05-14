75a82ad8598bd7cf4f034ceabcd9bdaa83d8cbe8
Abdelkarim Elmostakim <abdelkarimelmostakim07@gmail.com>
2024-05-14 11:44:37 -0700

    Prints the sum of the even-valued terms

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,25 +7,21 @@
  */
 int main(void)
 {
-	int i;
-	unsigned long int fib1 = 1, fib2 = 2, fib_sum;
+	long int fib1 = 1, fib2 = 2, fib_sum = 0, even_sum = 2;
 
-	printf("%lu, %lu", fib1, fib2);
-
-	for (i = 3; i <= 50; ++i)
+	while (fib_sum <= 4000000)
 	{
 		fib_sum = fib1 + fib2;
-		if (fib_sum < 4000000)
+		if (fib_sum % 2 == 0)
 		{
-			printf(", %lu", fib_sum);
-			fib1 = fib2;
-			fib2 = fib_sum;
+			even_sum += fib_sum;
 		}
-		else
-			break;
+
+		fib1 = fib2;
+		fib2 = fib_sum;
 	}
 
-	printf("\n");
+	printf("%ld\n", even_sum);
 
 	return (0);
 }
