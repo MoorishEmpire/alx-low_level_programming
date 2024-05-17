83048874f8d48875c9742ce8bf25ae5be0814616
Abdelkarim Elmostakim <abdelkarimelmostakim07@gmail.com>
2024-05-17 14:45:55 -0700

    Prints a triangle, followed by a new line

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,21 +9,21 @@
  */
 void print_triangle(int size)
 {
-    int i, j;
+	int i, j;
 
-    if (size <= 0)
-    {
-        _putchar('\n');
-    }
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
 
-    for (i = 0; i < size; i++)
-    {
-        for (j = 0; j < size - i - 1; j++)
-            _putchar(' ');
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+			_putchar(' ');
 
-        for (j = 0; j <= i; j++)
-            _putchar('#');
+		for (j = 0; j <= i; j++)
+			_putchar('#');
 
-        _putchar('\n');
-    }
+		_putchar('\n');
+	}
 }
