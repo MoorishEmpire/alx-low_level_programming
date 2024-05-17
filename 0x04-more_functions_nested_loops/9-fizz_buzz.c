8a632f092a507aeceb67520b3d0df04c2feef0e8
Abdelkarim Elmostakim <abdelkarimelmostakim07@gmail.com>
2024-05-17 13:52:58 -0700

    Fizz Buzz!

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,17 +14,18 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
-		{
 			printf("Fizz Buzz");
-		}
 		else if (i % 3 == 0)
 			printf("Fizz");
 		else if (i % 5 == 0)
 			printf("Buzz");
 		else
 			printf("%d", i);
-		if (i != 100)
+
+		if (i < 100)
 			printf(" ");
+		else
+			printf("\n");
 	}
 	return (0);
 }
