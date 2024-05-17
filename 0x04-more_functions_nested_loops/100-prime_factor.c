bae094d5a7daa80ca0e49425b8bd05632be64a1a
Abdelkarim Elmostakim <abdelkarimelmostakim07@gmail.com>
2024-05-17 16:43:49 -0700

    Lrgest prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * largest_prime_factor - Finds the largest prime factor of a number
+ * @n: The number to find the largest prime factor of
+ *
+ * Return: The largest prime factor of n
+ */
+long largest_prime_factor(long n)
+{
+	long largest_prime;
+	int i;
+
+	while (n % 2 == 0)
+	{
+		largest_prime = 2;
+		n /= 2;
+	}
+
+	for (i = 3; i <= sqrt(n); i += 2)
+	{
+		while (n % i == 0)
+		{
+			largest_prime = i;
+			n /= i;
+		}
+	}
+
+	if (n > 2)
+		largest_prime = n;
+
+	return (largest_prime);
+}
+
+/**
+  * main : Checks the code
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	long number;
+	long largest_prime;
+
+	number = 612852475143;
+	largest_prime = largest_prime_factor(number);
+
+	printf("%ld\n", largest_prime);
+
+	return (0);
+}
