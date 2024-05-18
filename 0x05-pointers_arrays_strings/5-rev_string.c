8e332362467467cbba8b3a2832c46be95e9c511f
Abdelkarim Elmostakim <abdelkarimelmostakim07@gmail.com>
2024-05-18 09:14:04 -0700

    A good  engineer thinks in reverse and asks himself about the stylistic

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,11 +10,11 @@ void rev_string(char *s)
 	int i;
 	char temp;
 
-	while (s[length] != '\0') {
+	while (s[length] != '\0')
 		length++;
-	}
 
-	for (i = 0; i < length / 2; i++) {
+	for (i = 0; i < length / 2; i++)
+	{
 		temp = s[i];
 		s[i] = s[length - 1 - i];
 		s[length - 1 - i] = temp;
