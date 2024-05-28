97d58ee1fb5b041e174622575930c7093a7d42a1
Abdelkarim Elmostakim <abdelkarimelmostakim07@gmail.com>
2024-05-27 18:35:38 -0700

    Dreams feel real while we're in them.

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,9 +1,11 @@
 #include "main.h"
 
 /**
-  * _strlen_recursion - Returns the length of a string.
-  * @s: The string to be printed.
-  */
+ * _strlen_recursion - Returns the length of a string.
+ * @s: The string to be printed.
+ *
+ * Return: Return the length of a string.
+ */
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
@@ -11,4 +13,3 @@ int _strlen_recursion(char *s)
 	else
 		return (1 + _strlen_recursion(s + 1));
 }
-	
