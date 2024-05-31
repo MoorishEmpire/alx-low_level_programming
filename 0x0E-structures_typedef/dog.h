79d604cbf24cad730b53d7569bed7d564fda15ba
Abdelkarim Elmostakim <abdelkarimelmostakim07@gmail.com>
2024-05-31 15:43:18 -0700

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+  * struct dog - Structre that take three elements.
+  * @name: The name of the dog.
+  * @age: The age of the dog.
+  * @owner: The name of the owner of the dog.
+  */
 struct dog
 {
 	char *name;
