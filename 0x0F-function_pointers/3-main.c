8a2be0eafab3449eaa05c1ea8c4d148b754ed06d
Abdelkarim Elmostakim <abdelkarimelmostakim07@gmail.com>
2024-06-02 04:32:13 -0700

    Sometimes you gotta fail to succeed

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -6,12 +6,12 @@
   * main - check the code.
   * @argc: The number of arguments.
   * @argv: The array of arguments.
-  * Return: 0 on success.
+  * Return: Always 0 (Success).
   */
 int main(int argc, char *argv[])
 {
-	int num1, num2, result;
-	int (*operation)(int, int);
+	int num1, num2;
+	int (*op_func)(int, int);
 
 	if (argc != 4)
 	{
@@ -21,21 +21,15 @@ int main(int argc, char *argv[])
 
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
-	operation = get_op_func(argv[2]);
+	op_func = get_op_func(argv[2]);
 
-	if (operation == NULL)
+	if (op_func == NULL)
 	{
 		printf("Error\n");
 		return (99);
 	}
-	if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
-	{
-		printf("Error\n");
-		return (100);
-	}
 
-	result = operation(num1, num2);
-	printf("%d\n", result);
+	printf("%d\n", op_func(num1, num2));
 
 	return (0);
 }
