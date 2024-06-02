acac6cf17a40b0bd8a429dcc3c85e8969290f786
Abdelkarim Elmostakim <abdelkarimelmostakim07@gmail.com>
2024-06-01 20:22:36 -0700

    Some times yougotta fails to succeed

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,60 @@
+#include "3-calc.h"
+
+/**
+  * op_add - Returns the sum of two integers.
+  * @a: The first integer.
+  * @b: The seconde integer.
+  *
+  * Return: The sum of a and b.
+  */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+  * op_sub - Returns the difference of two integers.
+  * @a: The first integer.
+  * @b: The seconde integer.
+  *
+  * Return: The difference of a and b.
+  */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+  * op_mul - Returns the product of two integers.
+  * @a: The first integer.
+  * @b: The seconde integer.
+  *
+  * Return: The product of a by b.
+  */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+  * op_div - Returns the division of two integers.
+  * @a: The first integer.
+  * @b: The seconde integer.
+  *
+  * Return: The division of a by b.
+  */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+  * op_mod - Returns the module of two integers.
+  * @a: The first integer.
+  * @b: The seconde integer.
+  *
+  * Return: The remainder of a by b.
+  */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
