f7657f5e71e566925fd1315898b7b5dafe1a7b5c
Abdelkarim Elmostakim <abdelkarimelmostakim07@gmail.com>
2024-06-03 13:49:48 -0700

    The Hare and the Tortoise

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,10 +3,14 @@
 void __attribute__((constructor)) before_main(void);
 
 /**
-  * befor-main - Function to print a message befor main is executed.
-  */
+ * before_main - Function to print a message before main is executed.
+ *
+ * Description: This function prints "You're beat! and yet, you must allow,\n
+ * I bore my house upon my back!" before the main function is executed.
+ */
 void before_main(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
+
