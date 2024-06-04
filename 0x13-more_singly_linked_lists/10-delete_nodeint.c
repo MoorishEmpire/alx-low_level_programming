47ad7e5b939dc0f96509aa73887fad94dab47cda
Abdelkarim Elmostakim <abdelkarimelmostakim07@gmail.com>
2024-06-04 14:22:41 -0700

    Delete at index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,45 +1,41 @@
 #include "lists.h"
-#include <stddef.h>
 #include <stdlib.h>
 
 /**
-  * delete_nodeint_at_index - Deletes the node at index of a listint_t list.
-  * @head: Pointer to the pointer of the first element of the listint_t.
-  * @index: The index of the node that should be deleted.
-  *
-  * Return: 1 if it succeeded, -1 if it failed.
-  */
+ * delete_nodeint_at_index - Deletes the node at index of a listint_t list.
+ * @head: Double pointer to the first element in the listint_t list.
+ * @index: The index of the node that should be deleted. Index starts at 0.
+ *
+ * Return: 1 if it succeeded, -1 if it failed.
+ */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *temp, *idxtemp;
 	unsigned int indx;
 
-	if (head == NULL && *head == NULL)
+	if (head == NULL || *head == NULL)
 		return (-1);
 
 	if (index == 0)
 	{
 		temp = *head;
-		*head = temp->next;
+		*head = (*head)->next;
 		free(temp);
-		temp = NULL;
 		return (1);
 	}
 
-	indx = index - 1;
 	temp = *head;
-	while (indx > 0 && temp->next != NULL)
+	for (indx = 0; indx < index - 1 && temp->next != NULL; indx++)
 	{
 		temp = temp->next;
-		indx--;
 	}
-	if (temp->next == NULL && indx != 0)
+
+	if (temp->next == NULL || indx != index - 1)
 		return (-1);
 
 	idxtemp = temp->next;
 	temp->next = temp->next->next;
 	free(idxtemp);
-	idxtemp = NULL;
 
 	return (1);
 }
