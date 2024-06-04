189988350ff77c52b5a757e54579448cd53674c1
Abdelkarim Elmostakim <abdelkarimelmostakim07@gmail.com>
2024-06-04 10:10:34 -0700

    Delete at index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -23,6 +23,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		*head = temp->next;
 		free(temp);
 		temp = NULL;
+		return (1);
 	}
 
 	indx = index - 1;
@@ -35,12 +36,10 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if (temp->next == NULL)
 		return (-1);
 
-	else
-	{
-		idxtemp = temp->next;
-		temp->next = (temp->next)->next;
-		free(idxtemp);
-		idxtemp = NULL;
-		return (1);
-	}
+	idxtemp = temp->next;
+	temp->next = (temp->next)->next;
+	free(idxtemp);
+	idxtemp = NULL;
+
+	return (1);
 }
