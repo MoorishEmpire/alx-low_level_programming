da3e6aa20f42b1e27a86c25ff0c9fb6db5c8be79
Abdelkarim Elmostakim <abdelkarimelmostakim07@gmail.com>
2024-06-03 18:16:38 -0700

    Free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,6 +10,8 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
+	if (head == NULL)
+		return;
 
 	while (*head != NULL)
 	{
@@ -17,4 +19,5 @@ void free_listint2(listint_t **head)
 		*head = temp->next;
 		free(temp);
 	}
+	*head = NULL;
 }
