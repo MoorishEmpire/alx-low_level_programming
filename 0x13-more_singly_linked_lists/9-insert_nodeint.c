5d35baffb4e6e9ddd3632d5dee82a977861cc0ad
Abdelkarim Elmostakim <abdelkarimelmostakim07@gmail.com>
2024-06-04 05:59:46 -0700

    Inset in the middle of the list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -25,9 +25,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	b->n = n;
 	b->next = NULL;
-	
+
 	if (idx == 0)
 	{
+		b->next = *head;
 		*head = b;
 		return (*head);
 	}
