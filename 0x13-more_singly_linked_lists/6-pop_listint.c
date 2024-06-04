0fa0fafc93f7c144ec6295499ced97d506d1079b
Abdelkarim Elmostakim <abdelkarimelmostakim07@gmail.com>
2024-06-03 19:20:36 -0700

    Pop

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -15,14 +15,13 @@ int pop_listint(listint_t **head)
 	listint_t *temp;
 	int n;
 
-	if (head == NULL)
+	if (head == NULL || *head == NULL)
 		return (0);
 
 	temp = *head;
 	*head = temp->next;
 	n = temp->n;
 	free(temp);
-	temp = NULL;
 
 	return (n);
 }
