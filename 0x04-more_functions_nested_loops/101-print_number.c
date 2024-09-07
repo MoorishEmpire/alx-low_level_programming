90ef70817982b2b002c4f9389b73b0aad1f4bac7
Abdelkarim Elmostakim <abdelkarimelmostakim07@gmail.com>
2024-09-07 10:36:46 -0700

    Numbers have life

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,25 +10,16 @@ void print_number(int n)
 	if (n == INT_MIN)
 	{
 		_putchar('-');
-		print_number((n / 10) * -1);
-		_putchar((n % 10) * -1 + '0');
+		_putchar('2');
+		n = 147483648;
 	}
-
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
-
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
 	}
-
-	if (n / 10 != 0)
+	if (n >= 10)
 		print_number(n / 10);
-
 	_putchar((n % 10) + '0');
 }
 
