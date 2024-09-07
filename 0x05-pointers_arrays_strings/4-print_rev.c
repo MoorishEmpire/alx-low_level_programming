89dda518e32594948a40f5ff75c9d387931b6209
Abdelkarim Elmostakim <abdelkarimelmostakim07@gmail.com>
2024-09-07 10:55:26 -0700

    Print reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,16 +7,15 @@
 void print_rev(char *s)
 {
 	int length = 0;
-	int i;
 
 	while (s[length] != '\0')
 	{
 		length++;
 	}
 
-	for (i = length - 1; i >= 0; i--)
+	while (--length >= 0)
 	{
-		_putchar(s[i]);
+		_putchar(s[length]);
 	}
 
 	_putchar('\n');
