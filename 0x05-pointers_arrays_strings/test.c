aae92910eb6b5f82bd5459e8292ed0c3d633815f
Abdelkarim Elmostakim <abdelkarimelmostakim07@gmail.com>
2024-09-07 13:37:44 -0700

    atoi

--- 0x05-pointers_arrays_strings/test.c
+++ 0x05-pointers_arrays_strings/test.c
@@ -1,20 +1,13 @@
-#include "main.h"
+#include <unistd.h>
 #include <limits.h>
 
-/**
-  * _atoi - Convert a string to an integer.
-  * @s: The string to be converted
-  *
-  * Return: Returns the integer converted
-  */
 int _atoi(char *s)
 {
 	int i;
 	int signe;
-	unsigned int number;
+	int number;
 
 	signe = 1;
-	number = 0;
 	i = 0;
 	while (s[i])
 	{
@@ -28,21 +21,14 @@ int _atoi(char *s)
 				|| (s[i] >= 'a' && s[i] <= 'z')
 				|| (s[i] >= 'A' && s[i] <= 'Z'))
 			i++;
+		number = 0;
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			while (s[i] >= '0' && s[i] <= '9')
 			{
-				if (number > (unsigned int)INT_MAX / 10
-						&& (s[i] - '0') > (INT_MAX % 10)
-						&& signe == -1)
-					return (INT_MIN);
-				if (number > (unsigned int)INT_MAX / 10
-						&& signe == 1)
-					 return (INT_MAX);
-
 				number *= 10;
 				number += s[i] - 48;
-							i++;
+				i++;
 			}
 			break;
 		}
