950232597da9eca0e953024dc1b0304f43950c9a
Abdelkarim Elmostakim <abdelkarimelmostakim07@gmail.com>
2024-09-11 09:27:37 -0700

    finally

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -2,6 +2,22 @@
 #include <string.h>
 
 /**
+  * digit_count - Counts the lenght of the two number and return the greatest.
+  * @n1: the first number.
+  * @n2: the second number.
+  *
+  * Return: The greatest len of the two numbers.
+  */
+
+int	digit_count(char *n1, char *n2)
+{
+	int	len1 = strlen(n1);
+	int	len2 = strlen(n2);
+
+	return	(len1 > len2 ? (len1) : (len2));
+}
+
+/**
  * infinite_add - Adds two numbers stored as strings.
  * @n1: The first number (string).
  * @n2: The second number (string).
@@ -10,40 +26,47 @@
  *
  * Return: A pointer to the result if it fits in the buffer, otherwise 0.
  */
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
+
+char	*infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-    int len1 = strlen(n1);
-    int len2 = strlen(n2);
-    int carry = 0, sum, i, j, k = 0;
+	int	len1;
+	int	len2;
+	int	carry;
+	int	sum;
+	int	i;
+	int	j;
+	int	k;
 
-    if (size_r <= len1 + 1 || size_r <= len2 + 1)
-        return (0);
+	len1 = strlen(n1);
+	len2 = strlen(n2);
+	carry = 0;
+	sum = 0;
+	i = len1 - 1;
+	j = len2 - 1;
+	k = 0;
+	if (size_r <= digit_count(n1, n2) + 1)
+		return (0);
 
-    r[size_r - 1] = '\0';
-    size_r--;
+	r[size_r - 1] = '\0';
 
-    for (i = len1 - 1, j = len2 - 1; i >= 0 || j >= 0 || carry; i--, j--, k++)
-    {
-        if (k >= size_r)
-            return (0);
+	for (k = size_r - 2; k >= 0; k--)
+	{
+		sum = carry;
+		if (i >= 0)
+			sum += n1[i--] - '0';
+		if (j >= 0)
+			sum += n2[j--] - '0';
+		if (sum == 0 && i < 0 && j < 0 && carry == 0)
+			break;
 
-        sum = carry;
-        if (i >= 0)
-            sum += n1[i] - '0';
-        if (j >= 0)
-            sum += n2[j] - '0';
+		carry = sum / 10;
 
-        carry = sum / 10;
-        r[size_r - 1 - k] = (sum % 10) + '0';
-    }
+		r[k] = (sum % 10) + '0';
+	}
+	if (carry > 0)
+	{
+		r[k--] = carry + '0';
+	}
 
-    k++;
-
-    if (k > size_r)
-        return (0);
-
-    memmove(r, r + size_r - k, k);
-
-    return (r);
+	return (r + k + 1);
 }
-
