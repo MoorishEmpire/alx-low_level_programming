2b30bb1104fd13815ce6f4bddedac35b058969d3
Abdelkarim Elmostakim <abdelkarimelmostakim07@gmail.com>
2024-09-14 07:19:57 -0700

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,14 +11,16 @@
   */
 char *_strchr(char *s, char c)
 {
-	while (*s++ != '\0')
+	while (*s != '\0')
 	{
 		if (*s == c)
 		{
-			break;
+			return (s);
 		}
+		s++;
 	}
-	if (*s == '\0')
-		return (NULL);
-	return (s);
+	if (c == '\0')
+		return (s);
+
+	return (NULL);
 }
