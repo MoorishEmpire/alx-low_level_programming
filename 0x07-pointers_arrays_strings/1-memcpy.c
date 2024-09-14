43dfc193555ee4f9a4f3398528e55a73a21ea484
Abdelkarim Elmostakim <abdelkarimelmostakim07@gmail.com>
2024-09-14 06:57:08 -0700

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,12 +11,10 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
-	char *dest2;
 
-	dest2 = dest;
 	for (i = 0; i < n; i++)
 	{
-		*dest++ = *src++;
+		dest[i] = src[i];
 	}
-	return (dest2);
+	return (dest);
 }
