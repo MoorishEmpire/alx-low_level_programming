779a770940f7b9dea79f67dcfd7595ce46256068
Abdelkarim Elmostakim <abdelkarimelmostakim07@gmail.com>
2024-09-14 16:52:08 -0700

    factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -6,12 +6,12 @@
   *
   * Return: Tha factorial calculated.
   */
-int factorial(int n)
+int	factorial(int n)
 {
-	if (n == 0 || n == 1)
-		return (1);
-	else if (n > 1)
-		return (n * factorial(n - 1));
-	else
+	if (n < 0)
 		return (-1);
+	if (n < 2)
+		return (1);
+	return (n * factorial(n - 1));
 }
+
