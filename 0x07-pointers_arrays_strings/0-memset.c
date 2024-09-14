ed103f99dc1ee778c60eca7052f88b62ae2ed3c1
Abdelkarim Elmostakim <abdelkarimelmostakim07@gmail.com>
2024-09-14 06:53:17 -0700

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,13 +11,11 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
-	char *str;
 
-	str = s;
 	for (i = 0; i < n; i++)
 	{
 		*(s + i) = b;
 	}
-	return (str);
+	return (s);
 }
 
