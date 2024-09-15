26dff91c06b5f0b5bfdbdaf24dc285b99b746118
Abdelkarim Elmostakim <abdelkarimelmostakim07@gmail.com>
2024-09-15 04:23:40 -0700

    palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,6 +1,23 @@
 #include "main.h"
 
 /**
+  * is_palindrome_helper - Helper for the main function.
+  * @s: the string to a char to be checked.
+  * @p1: the pointer to the begining of the string.
+  * @p2: the pointer to the end of the string.
+  *
+  * Return: 1 if the stirng is palindrome and 0 if not.
+  */
+int	is_palindrome_helper(char *s, char *p1, char *p2)
+{
+	if (*p1 != *p2)
+		return (0);
+	if (p1 > p2)
+		return (1);
+	return (is_palindrome_helper(s, p1 + 1, p2 - 1));
+}
+
+/**
   * is_palindrome - Functions that checks for palindromes.
   * @s: The string to a char to be checked.
   *
@@ -10,26 +27,12 @@ int	is_palindrome(char *s)
 {
 	char	*p1, *p2;
 	int	len;
-	int	is_palindrome;
 
-	is_palindrome = 1;
 	len =  0;
 	while (s[len])
 		len++;
 	p1 = s;
 	p2 = s + len - 1;
-	while (p1 < p2)
-	{
-		if (*p1 != *p2)
-		{
-			is_palindrome = 0;
-			break;
-		}
-		p1++;
-		p2--;
-	}
-	if (is_palindrome)
-		return (1);
-	return (0);
+	return (is_palindrome_helper(s, p1, p2));
 }
 
