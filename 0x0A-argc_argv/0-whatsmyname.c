3de154b551114644093a5eb52e627e0a01b7b990
Abdelkarim Elmostakim <abdelkarimelmostakim07@gmail.com>
2024-09-15 11:50:24 -0700

    my name is

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,18 +2,36 @@
 #include <unistd.h>
 #include <stdio.h>
 
+/**
+  * _putchar - Write the character that passed as an argument.
+  * @c: The char that should be writen
+  */
 void	_putchar(char c)
 {
 	write(1, &c, 1);
 }
 
+/**
+  * _putstr - Writes the string passed as an argument.
+  * @str: The string to be writen.
+  */
 void	_putstr(char *str)
 {
-	while (*str++)
+	while (*str)
+	{
 		_putchar(*str);
+		str++;
+	}
 	_putchar('\n');
 }
 
+/**
+  * main - check the code.
+  * @argc: Number of argument passed in the commande line.
+  * @argv: pointer to pointer to char.
+  *
+  * Return: 0 on Success.
+  */
 int	main(int argc, char *argv[])
 {
 	if (argc)
