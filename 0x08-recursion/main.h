91c930b88527ef116486075667b0eeb604efe0f3
Abdelkarim Elmostakim <abdelkarimelmostakim07@gmail.com>
2024-09-15 04:27:10 -0700

    main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -7,6 +7,7 @@
  * Return: Always 0.
  */
 int _putchar(char c);
+void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
@@ -15,7 +16,7 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_prime_helper(int n, int divisor);
 int is_palindrome(char *s);
-int is_palindrome_helper(char *s, int length, int i);
+int is_palindrome_helper(char *s, char *p1, char *p2);
 int wildcmp(char *s1, char *s2);
 
 #endif /* MAIN_H */
