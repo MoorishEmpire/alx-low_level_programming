ea0951f70dd84937ec55e9009f030f5bc38b39cb
Abdelkarim Elmostakim <abdelkarimelmostakim07@gmail.com>
2024-09-15 04:41:33 -0700

    palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,6 +1,20 @@
 #include "main.h"
 
 /**
+  * _strlen - function that calculates the length of a string.
+  * @s: the stirng to be checked.
+  * @len: the length to be calculated.
+  *
+  * Return: the length of the string.
+  */
+int	_strlen(char *s, int len)
+{
+	if (s[len] == '\0')
+		return (0);
+	return (1 + _strlen(s, len + 1));
+}
+
+/**
   * is_palindrome_helper - Helper for the main function.
   * @s: the string to a char to be checked.
   * @p1: the pointer to the begining of the string.
@@ -28,9 +42,7 @@ int	is_palindrome(char *s)
 	char	*p1, *p2;
 	int	len;
 
-	len =  0;
-	while (s[len])
-		len++;
+	len =  _strlen(s, 0);
 	p1 = s;
 	p2 = s + len - 1;
 	return (is_palindrome_helper(s, p1, p2));
