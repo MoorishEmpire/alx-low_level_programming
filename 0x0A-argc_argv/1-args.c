375c6ca914801f7cdb95116918c2f59f55e52cb4
Abdelkarim Elmostakim <abdelkarimelmostakim07@gmail.com>
2024-09-15 13:09:11 -0700

    number of arguments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+  * main - Prints the number of arguments entered in the commande line.
+  * @argc: The number of arguments.
+  * @argv: The pointer to pointer to a char.
+  *
+  * Return: 0 on Success.
+  */
 int	main(int argc, char *argv[])
 {
 	if (argv[0])
