1ce688e7461b482ea691f8011739a1e5828ea9ef
Abdelkarim Elmostakim <abdelkarimelmostakim07@gmail.com>
2024-09-22 04:13:27 -0700

    sum them all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,23 +9,22 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	
-    int sum = 0;
-    va_list args;
-    unsigned int i;
+	int sum;
+	va_list args;
+	unsigned int i;
 
-    if (n == 0)
-        return 0;
+	if (n == 0)
+		return (0);
 
-    va_start(args, n);
+	va_start(args, n);
 
+	sum = 0;
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(args, int);
+	}
 
-    for (i = 0; i < n; i++)
-    {
-        sum += va_arg(args, int);
-    }
+	va_end(args);
 
-    va_end(args);
-
-    return sum;
+	return (sum);
 }
