82ae057c260d93730f2afa327e61e288ec8bece3
Abdelkarim Elmostakim <abdelkarimelmostakim07@gmail.com>
2025-04-02 11:17:27 -0700

    Binary to unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,10 +12,14 @@ unsigned int binary_to_uint(const char *b)
 	int	result;
 	int	i;
 
+	if (!b)
+		return (0);
 	result = 0;
 	i = 0;
 	while (b[i])
 	{
+		if (b[i] != 0 && b[i] != 1)
+			return (0);
 		result *= 2;
 		result += b[i] - '0';
 		i++;
