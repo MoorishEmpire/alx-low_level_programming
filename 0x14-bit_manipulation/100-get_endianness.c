3406fd2e201a058af32afe7dbdc502ce561f35ac
Abdelkarim Elmostakim <abdelkarimelmostakim07@gmail.com>
2025-04-12 09:01:07 -0700

    endian

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -7,16 +7,12 @@
  */
 int	get_endianness(void)
 {
-	int		i;
 	uint32_t	x = 0xa1b2c3d4;
 	uint8_t		*c = (uint8_t *)&x;
 
-	i = sizeof(unsigned long int) * 8 - 1;
-	while (i >= 4)
-	{
-		if ((x & (1UL << i)) != (*c & (1UL << i)))
-			return (1);
-		i--;
-	}
-	return (0);
+	if (*c == 0xd4)
+		return (1);
+	else
+		return (0);
+	
 }
