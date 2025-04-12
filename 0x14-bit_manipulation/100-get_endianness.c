86b7ae3571f4d6b1207ebc69c9586ffdaa21568e
Abdelkarim Elmostakim <abdelkarimelmostakim07@gmail.com>
2025-04-12 08:52:42 -0700

    Endian

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * get_endianness - Fuctions that checks the endianness.
+ *
+ * Return: 0 if big endian, 1 if little endian.
+ */
+int	get_endianness(void)
+{
+	int		i;
+	uint32_t	x = 0xa1b2c3d4;
+	uint8_t*	c = (uint8_t *)&x;
+
+	i = sizeof(unsigned long int) * 8 - 1;
+	while (i >= 4)
+	{
+		if ((x & (1UL << i)) != (*c & (1UL << i)))
+			return (1);
+		i--;
+	}
+	return (0);
+}
